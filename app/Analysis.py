d09c2bb19f395254889bf3cfd588553fc6a3fda0
samueltenka <iguavadon@gmail.com>
2015-01-17 13:55:11 -0500

    Update Analysis.py
    Fixed inconsistent use of (time, value) v. (value, time). Now everything is (time, value).
    e.g. the gradient of happier and happier sentences in the test example returns:
    [(-2.0, 3.466230845507713), (-1.0, 4.0389955024432505), (0.0, 4.653395269292922), (1.0, 5.344463240761279), (3.0, 6.741363211663819)]
    
    indicating that the sentences are predicted to become super happy in the future.

--- app/Analysis.py
+++ app/Analysis.py
@@ -7,20 +7,24 @@ import Interpolator
 #import json
 
 #def analyze(query_phrase, tweets_and_times, target_times):
-def analyze(tweets_and_times, target_times):
+def analyze(timed_tweets, target_times):
         ''' e.g. ('motors',
                   [('go general motors!', -6.2), ...],
                   [-8.0, -7.9, ..., 0.9, 1.0])   
             where time is measured in days from now'''
-        scores_and_times = [(Scorer.positivity(tweet), time)
-                            for (tweet, time) in tweets_and_times]
-        return [(t, Interpolator.interpolate(scores_and_times, t))
+        timed_scores = [(time, Scorer.positivity(tweet))
+                         for (time, tweet) in timed_tweets]
+        return [(t, Interpolator.predict(timed_scores, t))
                 for t in target_times]
         
 
+##test:
 '''
-print(analyze([('i am a fan of rainbows', -7.0),
-         ('death and despair and neglectful abandonment', -5.0),
-         ('yahoo! i love you, most beautiful and happy one!', -3.0)],
-        [-2.0, -1.0, 0.0, 1.0]))
+print(analyze([
+               (-7.0, 'death and despair and neglectful abandonment'),
+               (-5.0, 'mediocre mustard'),
+               (-3.0, 'i am a fan of rainbows'),
+               (-1.0, 'yahoo! i love you, most beautiful and happy one!'),
+               (1.0, 'very very very very delectable incredibly absorbing frolic')],
+        [-2.0, -1.0, 0.0, 1.0, 3.0]))
 '''
