288d9f12ae0a825b04a966eb287cfaed41ba3f15
samueltenka <iguavadon@gmail.com>
2015-01-17 03:06:01 -0500

    Create Interpolator.py
    Locally Weighted Linear Regression working:
    OUTPUT:
    [[ 1. ]
     [ 0.5]]        <------ indicating y = 1 + 0.5x
    1.6             <------ indicating x=1.2 --> y=1.6

---
+++ TwitterAnalysis/Interpolator.py
@@ -0,0 +1,25 @@
+from numpy.linalg import *
+from numpy import *
+from math import *
+
+def interpolate(datapoints, query):
+   '''e.g. if datapoints==[(0.0, 1.0), (2.0, 2.0), (4.0, 3.0), (6.0, 4.0)]
+               and query==1.2, then should return (1.2, 1.6)'''
+   phi = array([[1, x] for (x, y) in datapoints])
+   t = array([[y] for (x, y) in datapoints])
+   def closeness(x, tau=0.1): ## tau==0 => nearest neighbor; 
+      return exp(-x*x/(2*tau))## tau==infinity => unweighted regression
+   N = len(datapoints)
+   kappa = array(  [  [closeness(datapoints[i][0]) if i==j else 0.0
+                       for j in range(N)]
+                    for i in range(N)])
+   phi_t = transpose(phi)
+   #print(phi_t)
+   #print(phi)
+   #print(kappa)
+   #print(t)
+   weights = dot(dot(dot(inv(dot(dot(phi_t,kappa),phi)),  phi_t), kappa), t)
+   print(weights)
+   return dot(transpose(weights), array([[1], [query]]))[0][0]
+   
+print(interpolate([(0.0, 1.0), (2.0, 2.0), (4.0, 3.0), (6.0, 4.0)], 1.2))
