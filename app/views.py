3148f6dcb357d8be00288d3b42d2d515a7f19046
Peter Mauldin <petermauldin@utexas.edu>
2015-01-17 15:02:21 -0500

    Got graph working with data

--- app/views.py
+++ app/views.py
@@ -3,22 +3,33 @@ from app import app
 from .forms import LoginForm
 from Twitter import Twitter
 import numpy
-# from Analysis import analyze
+from Analysis import analyze
 
-# total = 0
+
+d = [[]]
+
+def listtups_to_listlists(lt):
+  return [[x, y] for (x, y) in lt]
+
+query = ""
+
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
 def index():
+    global query
+    global d
     form = LoginForm()
     if form.validate_on_submit():
         # print "###################"
         count = 200
         total = 0
+        query = form.query.data
+        print "Query: %s" % query
         t = Twitter()
         a = t.getTweets(form.query.data, count)
 
-        graph = analyze(a, [-1.0, 0.0, 1.0])
-        print graph
+        d = analyze(a, [-10.0, 0.0, 2.0])
+        # print graph
         # print a
         # flash(a)
         return redirect('/results')
@@ -28,5 +39,14 @@ def index():
 
 @app.route('/results')
 def results():
+  global query
+  global d
+  dataList = listtups_to_listlists(d)
+
+  print dataList
+  print query
+
   return render_template('results.html',
-                           title='Results')
+                           title='Results',
+                           q=query,
+                           data=dataList)
